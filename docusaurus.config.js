e6edea4131f97ec60f778f9f67688dbce04cb83c
Tal Hayut <talhayut.dev@gmail.com>
2020-07-21 16:21:20 +0300

    fix: footer cr

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -58,7 +58,7 @@ module.exports = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} Tal Hayut`,
+      copyright: `Tal Hayut © ${new Date().getFullYear()}`,
     },
   },
   presets: [
